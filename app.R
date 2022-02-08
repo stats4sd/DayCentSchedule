fd8e8724b5c5877c6b8af22115d0832b29cf4777
Sam <s.dumble@stats4sd.org>
2022-02-08 11:55:28 +0000

    translations to Spanish

--- app.R
+++ app.R
@@ -3,6 +3,10 @@ library(DT)
 library(openxlsx)
 library(tidyverse)
 library(lubridate)
+library(shiny.i18n)
+
+i18n <- Translator$new(translation_csvs_path =  "translation")
+i18n$set_translation_language("en")
 
 #validentries<-read.xlsx("dot100_variableLists.xlsx",1)
 #write.csv(validentries,"validentries.csv",row.names = FALSE)
@@ -11,27 +15,32 @@ validentries<-read.csv("validentries.csv")
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Create DayCent Scheduler File From Input"),
+    titlePanel(i18n$t("Create DayCent Scheduler File From Input"), windowTitle = NULL),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
           htmlOutput("downloader"),
-          fileInput("file1", "Upload Completed Excel Schedule Template",
-                    multiple = FALSE,
-                    accept = c(".xlsx")),
-          checkboxInput("header","Include header/footer in schedule?",value = TRUE),
+          uiOutput("selections"),
+          uiOutput("headerin"),
           uiOutput("fn"),
         uiOutput("dl")
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
+          shiny.i18n::usei18n(i18n),
+          div(style = "float: right;",
+              selectInput('selected_language',
+                          i18n$t("Language"),
+                          choices = i18n$get_languages(),
+                          selected = i18n$get_key_translation())
+          ),
           tabsetPanel(
-            tabPanel("View Schedule",
+            tabPanel(i18n$t("View Schedule"),
                      htmlOutput("warning"),
                      htmlOutput("schedule")),
-           tabPanel("View Inputs",
+           tabPanel(i18n$t("View Inputs"),
           DTOutput("data1"))
         )
     )
@@ -41,11 +50,29 @@ ui <- fluidPage(
 #file1<-"C:/Users/sdumb/Documents/scheduleFile_template_SV2_test.xlsx"
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
 
+  output$selections <- renderUI({
+    fileInput("file1", i18n$t("Upload Completed Excel Schedule Template"),
+              multiple = FALSE,
+              accept = c(".xlsx"))
+  })
+  output$headerin <- renderUI({
+    checkboxInput("header",i18n$t("Include header/footer in schedule?"),value = TRUE)
+  })
+  
+  
+  observeEvent(input$selected_language, {
+    # This print is just for demonstration
+    print(paste("Language change!", input$selected_language))
+    # Here is where we update language in session
+    shiny.i18n::update_lang(session, input$selected_language)
+  })
+  
   output$downloader<-renderText(
     
-    HTML('Download Blank Excel Template File: <a href="https://github.com/stats4sd/DayCentSchedule/raw/main/scheduleFile_template.xlsx">Here</a><br><br>')
+    HTML(paste(i18n$t('Download Blank Excel Template File:'),
+    '<a href="https://github.com/stats4sd/DayCentSchedule/raw/main/scheduleFile_template.xlsx">Here</a><br><br>'))
     
   )
   observeEvent(input$file1,{
@@ -71,10 +98,14 @@ server <- function(input, output) {
       message<-""
     }
     else{
-      message<-paste("<p style='color:red;'>WARNING: ",nrow(fails)," code(s) specified in lookup list that were not found in the global dictionary.<br>",
-                     "These are:<br>",paste(fails$code,collapse="<br>"),"<br><br>",
-                     "Schedule file has been created with these codes still included.
-                     Please check codes, and correct them if needed.<br></p>")
+      message<-paste("<p style='color:red;'>",
+                     i18n$t("WARNING"),":",
+                            nrow(fails),
+                     i18n$t("code(s) specified in lookup list that were not found in the global dictionary"),".",
+                     "<br>",
+                     i18n$t("These are:"),"<br>",paste(fails$code,collapse="<br>"),"<br><br>",
+                     i18n$t("Schedule file has been created with these codes still included.  Please check codes, and correct them if needed."),
+                     "<br></p>")
     }
     
     message
@@ -103,15 +134,15 @@ server <- function(input, output) {
     select(daycent,year,doy)
   
 
-  
+
 output$fn <- renderUI({
     req(is.null(out1)==FALSE)
-    textInput("filename","Enter Output File Name",value="myschedule")
+    textInput("filename",i18n$t("Enter Output File Name"),value="myschedule")
   })
     
   output$dl <- renderUI({
     req(is.null(out1)==FALSE)
-    downloadLink('downloadData', 'Download Daycent Schedule File')
+    downloadLink('downloadData',i18n$t('Download Daycent Schedule File'))
   })
   
   
@@ -120,7 +151,7 @@ output$fn <- renderUI({
     dl_vers1<-select(out1,-doy,-year)
     if(input$header==TRUE){
       col1<-c("[XXX]","[XXX]","[XXX].100",0,-1,"-1.00",-1,-1,-1,0,0,-1,1,"[XXX]","","","Year Month Option",
-              "[XXX]","[XXX]","[XXX]","[XXX]","12","0.01923","F","[XXX].wth","")
+              "[XXX]","[XXX]","[XXX]","[XXX]","12","0.08333","F","[XXX].wth","")
       col2<-c("Starting Year","Last Year","Site file name","Labeling type","Labeling Year","Microcosm",
               "CO2 Systems","pH effect","Soil Warming","N input scalar option","OMAD scalar option",
               "Climate scalar option","Initial system","Initial crop","Initial tree","","","Block",
@@ -175,7 +206,7 @@ output$fn <- renderUI({
       dl_vers<-select(out1,-doy,-year)
         if(input$header==TRUE){
           col1<-c("[XXX]","[XXX]","[XXX].100",0,-1,"-1.00",-1,-1,-1,0,0,-1,1,"[XXX]","","","Year Month Option",
-                  "[XXX]","[XXX]","[XXX]","[XXX]","12","0.01923","F","[XXX].wth","")
+                  "[XXX]","[XXX]","[XXX]","[XXX]","12","0.08333","F","[XXX].wth","")
           col2<-c("Starting Year","Last Year","Site file name","Labeling type","Labeling Year","Microcosm",
                   "CO2 Systems","pH effect","Soil Warming","N input scalar option","OMAD scalar option",
                   "Climate scalar option","Initial system","Initial crop","Initial tree","","","Block",
